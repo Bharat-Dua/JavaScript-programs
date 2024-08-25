47c0fb25f98c38b0e833ac7213635024192f82e3
Bharat Dua <bharatdua13@gmail.com>
2024-08-26 00:47:38 +0530

    print hello world

---
+++ Print-hello-world.js
@@ -0,0 +1,5 @@
+//* ------- Print hello world -------
+
+//? The "Hello world" program is a classic in programming and is often the first program written when learning a new programming language.In this case,the goal is to print the text string "Hello,world" to the browser console.
+
+console.log("Hello world");
