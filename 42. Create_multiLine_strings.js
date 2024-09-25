351f53e352b106ac4634a86436768f44b9f651da
Bharat-Dua <bharatdua13@gmail.com>
2024-09-25 14:44:07 +0530

    create multiline strings

---
+++ 42. Create_multiLine_strings.js
@@ -0,0 +1,10 @@
+//*----- Create multiline strings
+
+//? This program creates multiline strings using template literals(introduced in ES6)
+
+let multilineString = `
+love to build web apps
+love problem solving
+always keep eye on to learn something`;
+
+console.log(multilineString);
