0bc7364103b9c2bc2c9f8402d3ed9eb16e2c60fa
Bharat-Dua <bharatdua13@gmail.com>
2024-09-25 16:57:04 +0530

    trim a string

---
+++ 46. Trim_a_string.js
@@ -0,0 +1,9 @@
+//*-------- Trim a string
+
+//? This program trim leading and trailing whitespaces from a string using the trim() method.
+
+function trimString(str) {
+  console.log(str.trim());
+  return str.trim();
+}
+trimString("       frontend            developer       ");
