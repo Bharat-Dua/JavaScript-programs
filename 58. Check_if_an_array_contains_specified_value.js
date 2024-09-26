3e5de33b4a79a7dba867257b13ea00c1f8e54b3b
Bharat-Dua <bharatdua13@gmail.com>
2024-09-26 17:30:15 +0530

    check if an array contains specified value

---
+++ 58. Check_if_an_array_contains_specified_value.js
@@ -0,0 +1,7 @@
+//*--------- check if an array contains specified value
+
+//? check if an array contains specified value in javascript, you can use includes() or indexOf() method.
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let checkItem = arr.includes(3);
+console.log(checkItem);
