7cd3a027968c5e2701cab813a0edb76e2fba9642
Bharat-Dua <bharatdua13@gmail.com>
2024-09-27 18:16:08 +0530

    write to console

---
+++ 79. Write_to_console.js
@@ -0,0 +1,12 @@
+//*---- write to console
+
+//? Writing to console in javascript is typically done using the console.log() method.
+
+console.log("Hello,world!");
+
+let num = 12;
+console.log(num);
+
+let fName = "Bharat";
+let lName = "dua";
+console.log("fullname:", fName, lName);
