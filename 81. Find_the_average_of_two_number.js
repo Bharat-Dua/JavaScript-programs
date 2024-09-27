94a869957d6696a5a1a3cf7431fd4a02fefad37a
Bharat-Dua <bharatdua13@gmail.com>
2024-09-27 19:01:05 +0530

    find the average of two number

---
+++ 81. Find_the_average_of_two_number.js
@@ -0,0 +1,10 @@
+//*------- find the average of two number
+
+//? To find the average of two numbers in javascript,you can add the two numbers together and then divide the sum by 2.
+
+function averageOfTwoNumbers(num1, num2) {
+  let sum = num1 + num2;
+  let average = sum / 2;
+  return average;
+}
+console.log(averageOfTwoNumbers(10, 20)); // Output: 15
