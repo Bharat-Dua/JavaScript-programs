516bd0fec41b37958c86b91231933c349b10dfe7
Bharat-Dua <bharatdua13@gmail.com>
2024-09-27 08:56:44 +0530

    work with constants

---
+++ 69. Work_with_constansts.js
@@ -0,0 +1,9 @@
+//*------- work with constants
+
+//? You can work with constants using const keyword to declare variables whose values should not be reassigned.
+
+const PI = 3.14159;
+
+PI = 4; // throw an error
+
+console.log(PI);
