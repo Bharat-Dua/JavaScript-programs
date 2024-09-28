8305ad89257412ab6ab7ff9098ce37e70a674d5a
Bharat-Dua <bharatdua13@gmail.com>
2024-09-28 13:11:42 +0530

    reverse an array

---
+++ 87. Reverse_an_array.js
@@ -0,0 +1,9 @@
+//*-------- Reverse an array
+
+//? To reverse an array in javascript,you can use the reverse() method,which is available for arrays.This method reverse the elements of an array in place.
+
+function reverseArray(arr) {
+  return arr.reverse();
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
