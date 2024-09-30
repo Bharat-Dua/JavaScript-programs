638cd9aa2bcfa82018d083d492bbe1b3791eb555
Bharat-Dua <bharatdua13@gmail.com>
2024-09-30 20:13:35 +0530

    find the area of a rectangle

---
+++ 105. Find_the_area_of_a_rectangle.js
@@ -0,0 +1,10 @@
+//*---- find the area of rectangle
+
+//? To find the area of a reactangle in javascript,you can create a function that takes the length and width as parameters and applies the formula.
+
+function findArea(length, width) {
+  let area = length * width;
+  return area;
+}
+console.log(findArea(5, 10)); // Output: 50
+console.log(findArea(10, 5.5)); // Output: 55
