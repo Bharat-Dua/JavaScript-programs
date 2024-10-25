3878abfb68b653dcf2a51253d9c7e41a15dbaa82
Bharat-Dua <bharatdua13@gmail.com>
2024-10-25 21:36:10 +0530

    find the area of a parallelogram

---
+++ 122. Find_the_area_of_a_parallelogram.js
@@ -0,0 +1,9 @@
+//*---- find the area of a parallelogram
+
+//? This program calculates the area of a parallelogram given the length of its base and the corresponding perpendicular height.
+
+function parallelogramArea(base, height) {
+  return base * height;
+}
+
+console.log(parallelogramArea(4, 5));
